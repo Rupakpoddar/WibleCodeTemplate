4e321b5c694822990dd18240b1147316a26c4593
Rupak Poddar <144166964+rupakpoddaradmt@users.noreply.github.com>
2025-04-15 11:26:14 -0400

    Update SerialMonitor.ino

--- Arduino/SerialMonitor/SerialMonitor.ino
+++ Arduino/SerialMonitor/SerialMonitor.ino
@@ -7,25 +7,21 @@
 #include <ArduinoBLE.h>
 
 #ifndef _WIBLE_ENABLED_
-  #error "Please install the modified ArduinoBLE library from: >>> https://github.com/Rupakpoddar/WibleCodeTemplate/raw/master/Arduino/ArduinoBLE.zip <<<"
+  #error "Please install the modified ArduinoBLE library from: https://github.com/Rupakpoddar/WibleCodeTemplate/raw/master/Arduino/ArduinoBLE.zip"
 #endif
 
-/*
-  * Create BLE service & characteristic to allow remote device to read, write, and notify
-*/
+// Create BLE service & characteristic to allow remote device to read, write, and notify
 BLEService TX_RX_Service("19B10010-E8F2-537E-4F6C-D104768A1214");
-BLECharacteristic TX_RX_Characteristic("19B10011-E8F2-537E-4F6C-D104768A1214", BLERead | BLEWriteWithoutResponse | BLENotify, 20);
+BLECharacteristic TX_RX_Characteristic("19B10011-E8F2-537E-4F6C-D104768A1214", 
+                                        BLERead | BLEWriteWithoutResponse | BLENotify, 20);
 
-/*
-  * Initialize counter
-*/
+// Initialize counter and timing variables
 int counter = 0;
+unsigned long previousMillis = 0; // last time the event occurred
+const long interval = 1000; // interval in milliseconds
 
-/*
-  * Implement delay without blocking
-*/
-unsigned long previousMillis = 0; // Stores the last time the event occurred
-const long interval = 1000; // Interval for the delay in milliseconds
+// Flag to detect changes in connection status
+bool deviceConnected = false;
 
 void setup() {
   Serial.begin(9600);
@@ -36,76 +32,62 @@ void setup() {
 
   // Initialize BLE module
   if (!BLE.begin()) {
-    Serial.println("BLE Initialization failed!");
-
+    Serial.println("BLE initialization failed!");
     while (1);
   }
 
-  // Set the local and device name this peripheral advertises (default: Arduino)
+  // Configure BLE device parameters
   BLE.setLocalName("Wible");
   BLE.setDeviceName("Wible");
-
-  // Set the UUID for the service this peripheral advertises
   BLE.setAdvertisedService(TX_RX_Service);
 
-  // Add the characteristics to the service
+  // Add the characteristic to the service and add the service
   TX_RX_Service.addCharacteristic(TX_RX_Characteristic);
-
-  // Add the service
   BLE.addService(TX_RX_Service);
 
-  // Start advertising
+  // Start advertising BLE service
   BLE.advertise();
-
-  Serial.println("BLE device active, waiting for connections...\n");
+  Serial.println("Device initialized. Advertising...");
 }
 
 void loop() {
-  /*
-    * Poll for BLE events
-  */
-  BLE.poll();
+  // Get the central device object, if any central is connected it will be non-null
+  BLEDevice central = BLE.central();
 
-  /*
-    * Check if BLE device is connected
-  */
-  if (BLE.connected()) {
-    /*
-      * Check if there is any data available from BLE device
-    */
+  if (central) {
+    // A central is connected now. Print connection message once.
+    if (!deviceConnected) {
+      Serial.println("Device connected.");
+      deviceConnected = true;
+    }
+
+    // If new data is written by the central, print it out.
     if (TX_RX_Characteristic.written()) {
-      // Get the data as a pointer to uint8_t (byte array)
       const uint8_t* value = TX_RX_Characteristic.value();
-
-      // Convert the byte array to a String
       String incomingData = String((char*)value);
-
-      // Print the received data to the Serial Monitor
       Serial.print("Received: ");
       Serial.println(incomingData);
     }
 
-    unsigned long currentMillis = millis(); // Get the current time
-
-    /*
-      * Check if the interval has passed
-    */
+    // Check if it is time to send a counter update to the central.
+    unsigned long currentMillis = millis();
     if (currentMillis - previousMillis >= interval) {
-      // Save the last time the event occurred
       previousMillis = currentMillis;
-
-      /*
-        * Write the counter value to remote device
-      */
-      TX_RX_Characteristic.writeValue(("Counter value: " + String(counter) + "\n").c_str());
-      
-      // Increment the counter
+      String msg = "Counter value: " + String(counter) + "\n";
+      // Write counter message to the central
+      TX_RX_Characteristic.writeValue(msg.c_str());
       counter++;
     }
+    // Optionally, signal connection via LED (steady ON)
+    digitalWrite(LED_BUILTIN, HIGH);
   } else {
-    /*
-      * Blink the builtin LED while the BLE device is waiting for connection
-    */
+    // No central connected. If we were previously connected, note the disconnect and restart advertising.
+    if (deviceConnected) {
+      Serial.println("Device disconnected. Re-advertising...");
+      deviceConnected = false;
+      BLE.advertise();
+    }
+    // Blink LED while waiting for connection
     digitalWrite(LED_BUILTIN, LOW);
     delay(250);
     digitalWrite(LED_BUILTIN, HIGH);
